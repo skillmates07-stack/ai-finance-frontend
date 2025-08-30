695033ef60f2d3383bc984d9ac99aa8f604f5857
skillmates07-stack <skillmates07@gmail.com>
2025-08-31 00:41:46 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,16 +3,13 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
-  
   images: {
     unoptimized: true,
   },
-  
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
-  
   poweredByHeader: false,
   reactStrictMode: true,
   swcMinify: true,
